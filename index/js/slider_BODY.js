1be1cc2cc59b495756d09f3197ec27577ed5ebdf
Adam Hoeber <33702699+HoeberAdam@users.noreply.github.com>
2018-01-17 19:40:05 -0600

    Add files via upload

---
+++ index/js/slider_BODY.js
@@ -0,0 +1,13 @@
+var slideshow=new TINY.slider.slide('slideshow',{
+	id:'slider',
+	auto:4,
+	resume:false,
+	vertical:false,
+	navid:'pagination',
+	activeclass:'current',
+	position:0,
+	rewind:false,
+	elastic:true,
+	left:'slideleft',
+	right:'slideright'
+});
